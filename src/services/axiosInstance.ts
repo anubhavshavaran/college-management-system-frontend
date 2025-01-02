c9782eeb6af3a145d49d3de7a361e18a67f0b32a
Anubhav <anubhavshavaran5678@gmail.com>
2025-01-02 18:48:24 +0530

    Changed the Backend Endpoint

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://college-management-system-backend-lbsp.onrender.com/api",
     timeout: 5000,
     headers: {'X-Custom-Header': 'foobar'},
     validateStatus: function () {
